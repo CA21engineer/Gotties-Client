ddfb0a740bfa8b81000ebd559d84f6f015db3bfb
ryoya ito <horusu.dqx@gmail.com>
2020-05-10 22:15:50 +0900

    users test

--- firebase/__test__/users.test.ts
+++ firebase/__test__/users.test.ts
@@ -35,4 +35,35 @@ describe('articles document test', () => {
     const db = authenticatedApp(auth);
     firebase.assertFails(db.collection('users').doc(user2.id).get());
   });
+
+  test('ログインしているユーザーなら作成できる', async () => {
+    const auth: Auth = {
+      uid: 'abc123',
+      email: '',
+    }
+    const db = authenticatedApp(auth);
+    firebase.assertSucceeds(db.collection('users').add({}));
+  });
+
+  test('ログインしているユーザーでも更新できない', async () => {
+    const admin = adminApp();
+    const user = await admin.collection('users').add({});
+    const auth: Auth = {
+      uid: user.id,
+      email: '',
+    }
+    const db = authenticatedApp(auth);
+    firebase.assertFails(db.collection('users').doc(user.id).update({}));
+  });
+
+  test('ログインしているユーザーなら削除できる', async () => {
+    const admin = adminApp();
+    const user = await admin.collection('users').add({});
+    const auth: Auth = {
+      uid: user.id,
+      email: '',
+    }
+    const db = authenticatedApp(auth);
+    firebase.assertSucceeds(db.collection('users').doc(user.id).delete());
+  });
 });
