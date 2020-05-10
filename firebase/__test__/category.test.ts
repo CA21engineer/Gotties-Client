43d1a8535c00ad2bf719f446554b90dd283a1dd3
ryoya ito <horusu.dqx@gmail.com>
2020-05-11 00:04:22 +0900

    category rules test

--- firebase/__test__/category.test.ts
+++ firebase/__test__/category.test.ts
@@ -1,6 +1,6 @@
-import { firebase, projectID, rules, authenticatedApp } from './firestore_test.util';
+import { firebase, projectID, rules, authenticatedApp, Auth, adminApp } from './firestore_test.util';
 
-describe('articles document test', () => {
+describe('category document test', () => {
   beforeAll(async () => {
     await firebase.loadFirestoreRules({ projectId: projectID, rules })
   });
@@ -13,8 +13,89 @@ describe('articles document test', () => {
     await Promise.all(firebase.apps().map((app: any) => app.delete()))
   });
 
-  test('認証してないユーザーでもarticleを取得できる', async () => {
+  it('認証してないユーザーでもcategoryを取得できる', async () => {
     const db = authenticatedApp(null);
-    firebase.assertSucceeds((await db.collection('category').get()).docs);
+    await firebase.assertSucceeds((await db.collection('category').get()).docs);
+  });
+
+  it('認証ユーザーでかつタイトルがあれば登録できる', async () => {
+    const auth: Auth = {
+      uid: 'abc123',
+      email: '',
+    }
+    const db = authenticatedApp(auth);
+    await firebase.assertSucceeds(db.collection('category').add({
+      title: 'title',
+    }));
+  });
+
+  it('タイトルがないと登録できない', async () => {
+    const auth: Auth = {
+      uid: 'abc123',
+      email: '',
+    }
+    const db = authenticatedApp(auth);
+    await firebase.assertFails(db.collection('category').add({}));
+  });
+
+  it('認証ユーザーでない場合登録できない', async () => {
+    const db = authenticatedApp(null);
+    await firebase.assertFails(db.collection('category').add({}));
+  });
+  
+  it('認証ユーザーでかつタイトルがあれば更新できる', async () => {
+    const admin = adminApp();
+    const auth: Auth = {
+      uid: 'abc123',
+      email: '',
+    }
+    const category = await admin.collection('category').add({
+      title: 'title',
+      reading: 're',
+    });
+    const db = authenticatedApp(auth);
+    await firebase.assertSucceeds(db.collection('category').doc(category.id).update({
+      title: 'title_updated',
+    }));
+  });
+
+  it('認証ユーザーでない場合更新できない', async () => {
+    const admin = adminApp();
+    const category = await admin.collection('category').add({
+      title: 'title',
+    });
+    const db = authenticatedApp(null);
+    await firebase.assertFails(db.collection('category').doc(category.id).update({
+      title: 'title_updated',
+      reading: 'reading',
+    }));
+  });
+
+  it('タイトルがないと更新できない', async () => {
+    const admin = adminApp();
+    const auth: Auth = {
+      uid: 'abc123',
+      email: '',
+    }
+    const category = await admin.collection('category').add({
+      title: 'title',
+    });
+    const db = authenticatedApp(auth);
+    await firebase.assertFails(db.collection('category').doc(category.id).update({
+      reading: 'reading',
+    }));
+  });
+
+  it('削除できない', async () => {
+    const admin = adminApp();
+    const auth: Auth = {
+      uid: 'abc123',
+      email: '',
+    }
+    const category = await admin.collection('category').add({
+      title: 'title',
+    });
+    const db = authenticatedApp(auth);
+    await firebase.assertFails(db.collection('category').doc(category.id).delete());
   });
 });
