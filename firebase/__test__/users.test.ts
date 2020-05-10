59c6e4c0a2e58708e1435a10854715726f0fe53a
ryoya ito <horusu.dqx@gmail.com>
2020-05-10 22:27:47 +0900

    like_articles test

--- firebase/__test__/users.test.ts
+++ firebase/__test__/users.test.ts
@@ -66,4 +66,89 @@ describe('articles document test', () => {
     const db = authenticatedApp(auth);
     firebase.assertSucceeds(db.collection('users').doc(user.id).delete());
   });
+
+  describe('like_users test', () => {
+    test('ログインユーザーのドキュメントのサブコレクションの場合読める', async () => {
+      const admin = adminApp();
+      const user = await admin.collection('users').add({});
+      const auth: Auth = {
+        uid: user.id,
+        email: '',
+      }
+      const db = authenticatedApp(auth);
+      firebase.assertSucceeds((await db.collection('users').doc(user.id).collection('like_articles').get()).docs);
+    });
+
+    test('ログインユーザーのドキュメントのサブコレクションでない場合読めない', async () => {
+      const admin = adminApp();
+      const user = await admin.collection('users').add({});
+      const user2 = await admin.collection('users').add({});
+      const auth: Auth = {
+        uid: user.id,
+        email: '',
+      }
+      const db = authenticatedApp(auth);
+      firebase.assertFails(db.collection('users').doc(user2.id).collection('like_articles').get());
+    });
+
+    test('ログインユーザーのドキュメントのサブコレクションの場合作成できる', async () => {
+      const admin = adminApp();
+      const user = await admin.collection('users').add({});
+      const auth: Auth = {
+        uid: user.id,
+        email: '',
+      }
+      const db = authenticatedApp(auth);
+      firebase.assertSucceeds(db.collection('users').doc(user.id).collection('like_articles').add({}));
+    });
+
+    test('ログインユーザーのドキュメントのサブコレクションでない場合作成できない', async () => {
+      const admin = adminApp();
+      const user = await admin.collection('users').add({});
+      const user2 = await admin.collection('users').add({});
+      const auth: Auth = {
+        uid: user.id,
+        email: '',
+      }
+      const db = authenticatedApp(auth);
+      firebase.assertFails(db.collection('users').doc(user2.id).collection('like_articles').get());
+    });
+
+    test('ログインユーザーのドキュメントのサブコレクションでも更新できない', async () => {
+      const admin = adminApp();
+      const user = await admin.collection('users').add({});
+      const auth: Auth = {
+        uid: user.id,
+        email: '',
+      }
+      const likeArticle = await admin.collection('users').doc(user.id).collection('like_articles').add({});
+      const db = authenticatedApp(auth);
+      firebase.assertFails(db.collection('users').doc(user.id).collection('like_articles').doc(likeArticle.id).update({}));
+    });
+
+    test('ログインユーザーのドキュメントのサブコレクションの場合削除できる', async () => {
+      const admin = adminApp();
+      const user = await admin.collection('users').add({});
+      const auth: Auth = {
+        uid: user.id,
+        email: '',
+      }
+      const likeArticle = await admin.collection('users').doc(user.id).collection('like_articles').add({});
+      const db = authenticatedApp(auth);
+      firebase.assertSucceeds(db.collection('users').doc(user.id).collection('like_articles').doc(likeArticle.id).delete());
+    });
+
+    test('ログインユーザーのドキュメントのサブコレクションでない場合削除できない', async () => {
+      const admin = adminApp();
+      const user = await admin.collection('users').add({});
+      const user2 = await admin.collection('users').add({});
+      const auth: Auth = {
+        uid: user.id,
+        email: '',
+      }
+      const likeArticle = await admin.collection('users').doc(user.id).collection('like_articles').add({});
+      const db = authenticatedApp(auth);
+      firebase.assertFails(db.collection('users').doc(user2.id).collection('like_articles').doc(likeArticle.id).delete());
+    });
+  });
 });
